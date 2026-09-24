9b304a9461eca4f832f562aa867e0ef131b2027f
Устюгова <ust.mdd@gmail.com>
2026-09-24 13:29:00 +0300

    переделала 2

--- PointMutations.py
+++ PointMutations.py
@@ -1,14 +1,18 @@
-from random import *
+# from random import *
 
-bases='ATCG'
-l=randint(1,1000)
-s1=''.join(choice(bases) for _ in range(l))
-s2=''.join(choice(bases) for _ in range(l))
-print(s1)
-print(s2)
+# bases='ATCG'
+# l=randint(1,1000)
+# s1=''.join(choice(bases) for _ in range(l))
+# s2=''.join(choice(bases) for _ in range(l))
+# print(s1)
+# print(s2)
+
+with open('CPM.txt','r') as file:
+    s1=file.readline()
+    s2=file.readline()
 
 count=0
-for i in range(l):
+for i in range(len(s1)):
     if s1[i]!=s2[i]:
         count+=1
 print(count)
